ff20d7631124d5b0f987326b8405f0cf606f9b79
Monomee <hoangthth12@gmail.com>
2024-11-20 00:56:43 +0700

    currencyConverter

---
+++ CurrencyConverter/CurrencyConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double rate = 23000d;
+            Console.Write("Enter USD: ");
+            double usd = double.Parse(Console.ReadLine());
+            Console.WriteLine(usd + " USD -> " + (usd * rate) + " VND");
+            Console.ReadLine();
+        }
+    }
+}
